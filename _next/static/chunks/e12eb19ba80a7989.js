583715b92b6e0091ff52971604c045079d4be586
Leo <ywang277@usc.edu>
2025-09-22 11:15:51 -0700

    Deploy static site

---
+++ _next/static/chunks/e12eb19ba80a7989.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/09be5bd4d5f711cb.js",
+  "static/chunks/7f483907c67a4abf.js",
+  "static/chunks/turbopack-1d1be84c53ba39c1.js"
+])
---
+++ _next/static/chunks/fa7d2e6ac94c955b.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/7d854b9474a2b97e.js",
+  "static/chunks/7f483907c67a4abf.js",
+  "static/chunks/turbopack-ce5379bd690bcc99.js"
+])
